3cb8ecda6109b6c25695b34c39958933e9ff05ab
Bartek <bartosz.rychlicki@hint.pl>
2012-08-31 09:54:18 +0200

    kindle mode, externalise settings, clean

--- mlm.py
+++ mlm.py
@@ -1,23 +1,14 @@
 #!/usr/bin/env python
 from Tkinter import *
-from evdev import InputDevice, ecodes, event_factory
+from evdev import InputDevice, ecodes
 from select import select
 
 import codecs
-import subprocess
 import re
+from local_settings import *
 
 # config section
-D = DEBUG = True
-CUT_ENDINGS = True
-MIN_WORD_LENGTH = 3
-MAX_WORD_LENGTH = 20
-DISPLAY_RESULTS = 40
-DICS = [
-    '/home/bartek/workspace/mlm/dics/wiktionary_en_es.tsv',
-    '/home/bartek/workspace/mlm/dics/my.tsv'
- ]
-DEV = InputDevice('/dev/input/by-path/pci-0000:00:1a.1-usb-0:2:1.0-event-mouse')
+
 
 
 # http://casa.colorado.edu/~ginsbura/pygrep.htm
@@ -26,17 +17,13 @@ def grepf(string, list):
     return filter(expr.search, list)
 
 
+
 lines = []
 for filename in DICS:
     f = codecs.open(filename, 'r', 'utf-8')
     lines += f.readlines()
 
-
-class SynEvent(object):
-    def __init__(self, event):
-        print 'zorro'
-
-event_factory[ecodes.EV_KEY] = SynEvent
+print lines[26294:26300]
 
 
 class App:
@@ -45,9 +32,9 @@ class App:
 
         self.root = Tk()
         self.root.wm_title("mlm")
-        self.root.geometry("286x1050+1394+0")
+        self.root.geometry(GEOMETRY)
 
-        self.text = Text(self.root, width=40, height=80)
+        self.text = Text(self.root, width=40, height=ROWS)
         self.text.pack()
         self.text.tag_configure('head', font='helvetica 12 bold', background='green')
         self.text.tag_configure('linked', font='helvetica 8 italic', lmargin1=10, lmargin2=10, rmargin=10)
@@ -64,7 +51,7 @@ class App:
                     what = self.root.selection_get().strip()
                     if D:
                         print 'what', what, len(what), type(what)
-                    if not MIN_WORD_LENGTH < len(what) < MAX_WORD_LENGTH:
+                    if not MIN_WORD_LENGTH <= len(what) <= MAX_WORD_LENGTH:
                         break
                 except:
                     if D:
@@ -75,18 +62,30 @@ class App:
                     self.text.delete("1.0", END)
                     if CUT_ENDINGS:
                         what = self.strip_plural_endings(what)
+                        print 'ending cut', what
+                    if REPLACE:
+                        what = self.mu_rep(what,
+                            # quetsionable
+                            {
+                            'ida': 'ido',
+                            'gua': 'guo'
+                            }
+                            )
                     if D:
                         inter = ['m:next group']
                     else:
                         inter = []
                     # probably this could be rewritten, one call and sort
-                    grep_output = grepf('^%s\s' % what, lines) + \
-                                  inter + \
-                                  grepf('^%s[^\s]' % what, lines) + \
-                                  inter + \
-                                  grepf('\s(?<!^)%s\s' % what, lines) + \
-                                  inter + \
-                                  grepf('\s(?<!^)%s[^\s]' % what, lines)
+                    if KINDLE_MODE:
+                        grep_output = grepf('(^|\t)%s\s' % what, lines)
+                    else:
+                        grep_output = grepf('^%s\s' % what, lines) + \
+                                      inter + \
+                                      grepf('^%s[^\s]' % what, lines) + \
+                                      inter + \
+                                      grepf('\s(?<!^)%s\s' % what, lines) + \
+                                      inter + \
+                                      grepf('\s(?<!^)%s[^\s]' % what, lines)
                     if D:
                         print 'found', len(grep_output), 'matches'
                         print 'grep_output', grep_output
@@ -113,6 +112,10 @@ class App:
     def strip_plural_endings(self, string):
         expr = re.compile('[e]{0,}s$', flags=re.U | re.I)
         return expr.sub('', string)
+
+    def mu_rep(self, string, replacements={}):
+        """multiple replace, string, dic => string; ex. dic = {'/*': '\t', '-\t': ''} """
+        return reduce(lambda a, (b, c): a.replace(b, c), replacements.items(), string)
 
 
 app = App()
